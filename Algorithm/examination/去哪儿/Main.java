3a914b01b43b215af9db9c80b8028d5a369e70a8
wanglei <wanglei@iyunxiao.com>
2018-09-23 11:06:45 +0800

    添加了《深入理解计算机原理》的内容

---
+++ Algorithm/examination/去哪儿/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    int result = 0;    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x1 = sc.nextLine();
+        int y1 = sc.nextLine();
+        int x2 = sc.nextLine();
+        int y2 = sc.nextLine();
+        sc.close();
+
+
+    }
+
+    public static int leastWay (int x1, int y1, int x2, int y2) {
+        
+    }
+}
